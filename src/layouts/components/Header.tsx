4330f5fcaa7c6175da1ed8a85f2ed734f2446b2f
Gustavs <gustavskuks@gmail.com>
2023-01-23 17:29:17 +0200

    Font size change for App name/home link.

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
     return (
         <>
-            <Link className="text-lg my-auto ml-4 font-bold text-text-icons" to="/">Frontend Learning - GFK</Link>
+            <Link className="text-base my-auto ml-4 font-bold text-text-icons" to="/">Frontend Learning - GFK</Link>
             <Link className="px-3 py-1 bg-accent rounded text-text-icons" to="/entry">Data entry</Link>
             <Link className="px-3 py-1 bg-accent rounded text-text-icons" to="/display">Data display</Link>
         </>
